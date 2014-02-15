9c8a226a26b3209224681cdf2635f4228489198c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-02-15 14:50:16 -0500

    lots of nasty schema mapping that will make things way easier to blow through.

---
+++ test/fixtures/buildInfo.js
@@ -0,0 +1,20 @@
+module.exports = {
+  version: "2.5.6-pre-",
+  gitVersion: "518cbb85a00e4e9ac7dc419569aacc3216db45d2",
+  OpenSSLVersion: "",
+  sysInfo: "Darwin Lucass-MacBook-Air.local 13.0.0 Darwin Kernel Version 13.0.0: Thu Sep 19 22:22:27 PDT 2013; root:xnu-2422.1.72~6/RELEASE_X86_64 x86_64 BOOST_LIB_VERSION=1_49",
+  loaderFlags: "-fPIC -pthread -Wl,-bind_at_load -mmacosx-version-min=10.6",
+  compilerFlags: "-Wnon-virtual-dtor -Woverloaded-virtual -fPIC -fno-strict-aliasing -ggdb -pthread -Wall -Wsign-compare -Wno-unknown-pragmas -Winvalid-pch -Werror -pipe -O3 -Wno-unused-function -Wno-unused-private-field -Wno-deprecated-declarations -Wno-tautological-constant-out-of-range-compare -mmacosx-version-min=10.6",
+  allocator: "tcmalloc",
+  versionArray: [
+  2,
+  5,
+  6,
+  -100
+  ],
+  javascriptEngine: "V8",
+  bits: 64,
+  debug: false,
+  maxBsonObjectSize: 16777216,
+  ok: 1
+};
