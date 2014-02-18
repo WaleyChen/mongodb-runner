acfcbc76075018ed41345854ebb238da1c8fca06
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-02-18 18:04:38 -0500

    Format system time nicely

--- app/controllers/info.js
+++ app/controllers/info.js
@@ -1,6 +1,7 @@
 var Backbone = require('Backbone'),
   $ = Backbone.$,
   models = require('../models'),
+  moment = require('moment'),
   debug = require('debug')('mongoscope:info');
 
 module.exports = Backbone.View.extend({
@@ -30,6 +31,7 @@ module.exports = Backbone.View.extend({
         'build': self.build.toJSON(),
         'host': self.host.toJSON()
       };
+      ctx.host.system_time = moment(ctx.host.system_time).format('MMM Do YYYY, h:mm:ss a');
       self.$el.html(self.tpl(ctx));
     });
   }
