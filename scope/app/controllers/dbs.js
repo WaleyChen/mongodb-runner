e631c082fa846e682ebfa7f9b95e6c0f3623ddf7
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-06 17:07:02 -0500

    make databases use new models and structs

--- scope/app/controllers/dbs.js
+++ scope/app/controllers/dbs.js
@@ -10,35 +10,20 @@ module.exports = Backbone.View.extend({
     this.$el = $('#mongoscope');
     this.el = this.$el.get(0);
 
-    this.databases = new models.Databases()
+    this.instance = models.instance
       .on('sync', this.render, this)
       .on('error', this.render, this);
   },
   activate: function(){
-    this.databases.fetch();
+    this.instance.fetch();
   },
   deactivate: function(){},
   render: function(){
     var self = this;
-    this.collections = new models.Collections({
-      models: _.flatten(self.databases.map(function(database){
-          return database.get('collections');
-      }))
-    });
-
-    this.indexes = new models.Indexes({
-      models: _.flatten(self.databases.map(function(database){
-          return database.get('indexes');
-      }))
-    });
-
     requestAnimationFrame(function(){
-      var ctx = {
-        'databases': self.databases.toJSON(),
-        'collections': self.collections.toJSON(),
-        'indexes': self.indexes.toJSON()
-      };
-      self.$el.html(self.tpl(ctx));
+      self.$el.html(self.tpl({
+        'instance': self.instance.toJSON()
+      }));
     });
   }
 });
