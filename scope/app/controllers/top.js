528c2492e5addcae9e821288a215163989c3efa4
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-08 15:04:37 -0500

    dem nested animation frames...

--- scope/app/controllers/top.js
+++ scope/app/controllers/top.js
@@ -2,7 +2,8 @@
 
 var Backbone = require('backbone'),
   models = require('../models'),
-  debug = require('debug')('mg:scope:top');
+  debug = require('debug')('mg:scope:top'),
+  creek = require('../creek');
 
 module.exports = Backbone.View.extend({
   tpl: require('../templates/top.jade'),
@@ -13,6 +14,8 @@ module.exports = Backbone.View.extend({
     this.top = new models.Top()
       .on('sync', this.render, this)
       .on('error', this.render, this);
+
+    this.graphs = [];
   },
   activate: function(){
     this.top.fetch();
@@ -27,9 +30,18 @@ module.exports = Backbone.View.extend({
     var self = this,
       ctx = self.top.toJSON();
 
+    if(this.top.hasChanged('namespaces')){
+      this.graphs = this.top.get('namespaces').map(function(ns, i){
+        return creek('#creek-' + i, {});
+      });
+    }
+
     process.nextTick(function(){
       debug('writing template');
       self.$el.html(self.tpl(ctx));
+      self.graphs.map(function(graph){
+        graph.render();
+      });
     });
     return self;
   }
--- scope/app/creek.js
+++ scope/app/creek.js
@@ -10,15 +10,15 @@
 // /**
 //  * data stream wrapper.
 //  */
-// .data-stream {
+// .creek {
 //   font-family: 'PT Sans','Helvetica Neue',Helvetica,Arial,sans-serif;
 //   color: #494747;
 // }
-// .data-stream-stage {}
+// .creek-stage {}
 // /**
 //  * A line of data.
 //  */
-// .data-stream-line {
+// .creek-line {
 //   fill: none;
 //   stroke: #000;
 //   stroke-width: 2px;
@@ -26,14 +26,14 @@
 // /**
 //  * The area under the line.
 //  */
-// .data-stream-area {
+// .creek-area {
 //   fill: red;
 //   stroke-width: 0;
 // }
-// /**
+// *
 //  * The tracking line.
-//  */
-// .data-stream path.domain {
+
+// .creek path.domain {
 //   fill: none;
 //   stroke-width: 1px;
 //   stroke: #CCC;
@@ -41,34 +41,40 @@
 // /**
 //  * axis text.
 //  */
-// .data-stream text {
+// .creek text {
 //   color: #CCC;
 //   font-size: 10px;
 // }
-// .data-stream .x-axis line, .data-stream .y-axis line {
+// .creek .x-axis line, .creek .y-axis line {
 //   stroke: #CCC;
 //   stroke-width: 1px;
 //   shape-rendering: crispEdges;
 // }
-// .data-stream .y-axis {
+// .creek .y-axis {
 //   opacity: .7;
 // }
 //```
-
 var _ = require('underscore'),
-  d3 = require('d3');
+  d3 = require('d3'),
+  debug = require('debug')('mg:creek');
+
+module.exports = function(selector, opts){
+  opts = opts || {};
+  opts.selector = selector;
+  return new Creek(opts);
+};
 
 function Creek(opts){
-  _.defaults({}, opts, {
+  _.extend(this, _.defaults({}, opts, {
     n: 25,
     duration: 500,
     count: 0,
     data: [],
     interpolation: 'step-after',
     width: 320,
-    height: 120,
-    paused: false
-  });
+    height: 160,
+    selector: 'body'
+  }));
 
   var self = this;
 
@@ -80,54 +86,61 @@ function Creek(opts){
 
   this.value = 0;
 
-  var now = new Date(Date.now() - this.duration);
-  this.scales.x = d3.time.scale()
-    .domain([now - (this.n - 2) * this.duration, now - this.duration])
-    .range([0, this.width]);
-
-  this.scales.y = d3.scale.linear()
-    .range([this.height, 0])
-    .domain([0, 0]);
-
   this.clipId = 'clip-' + Math.random();
+}
 
-  this.selection = d3.select('body').append('p');
+Creek.prototype.render = function(){
+  this.selection = d3.select(document.querySelector(this.selector));
 
-  this.svg = this.selection
+  this.svg = this.selection.append('p')
     .append('svg')
-      .attr('class', 'data-stream')
+      .attr('class', 'creek')
+      .attr('height', this.height + 20)
+      .attr('width', this.width)
     .append('g')
-      .attr('transform', 'translate(40,10)');
+      .attr('transform', 'translate(40,0)');
 
-  this.clip = this.svg.append('g')
-    .attr('class', 'data-stream-stage')
-    .attr('clip-path', 'url(#' + this.clipId + ')');
+  var self = this,
+    clip = this.svg
+    .append('g')
+      .attr('class', 'creek-stage')
+      .attr('clip-path', 'url(#' + this.clipId + ')');
+
+  var now = new Date(Date.now() - this.duration);
+  this.scales = {
+    x: d3.time.scale()
+      .domain([now - (this.n - 2) * this.duration, now - this.duration])
+      .range([0, this.width]),
+    y: d3.scale.linear()
+    .range([this.height, 0])
+    .domain([0, 0])
+  };
 
   this.shapes = {
     line: d3.svg.line().interpolate(this.interpolation)
       .x(function(d, i){
-        return self.scales.x(now - (this.n - 1 - i) * this.duration);
+        return self.scales.x(now - (self.n - 1 - i) * self.duration);
       })
       .y(function(d, i){
         return self.scales.y(d);
       }),
     area: d3.svg.area().interpolate(this.interpolation)
       .x(function(d, i){
-        return self.scales.x(now - (this.n - 1 - i) * this.duration);
+        return self.scales.x(now - (self.n - 1 - i) * self.duration);
       })
       .y(function(d, i){
         return self.scales.y(d);
       })
   };
 
-  this.area = this.clip.append('path')
+  this.area = clip.append('path')
     .data([this.data])
-    .attr('class', 'data-stream-area')
+    .attr('class', 'creek-area')
     .attr('d', this.shapes.area);
 
-  this.path = this.clip.append('path')
+  this.path = clip.append('path')
     .data([this.data])
-    .attr('class', 'data-stream-line')
+    .attr('class', 'creek-line')
     .attr('d', this.shapes.line);
 
 
@@ -141,9 +154,11 @@ function Creek(opts){
     .attr('transform', 'translate(0,0)')
     .call(this.scales.y.axis = d3.svg.axis().scale(this.scales.y).orient('left'))
   };
+  this.paused = false;
 
   this.tick();
-}
+  return this;
+};
 
 Creek.prototype.pause = function(i){
   this.paused = true;
@@ -165,53 +180,44 @@ Creek.prototype.tick = function(){
   var now = new Date(),
     self = this;
 
-  this.scales.x.domain([now - (this.n - 2) * this.duration, now - this.duration]);
-  this.scales.y.domain([0, d3.max(this.data)]);
-
   // get new value from the queue
   this.data.push(this.value);
   this.value = 0;
 
-  // reset the shapes
-  this.svg.select('.data-stream-line')
-      .attr('d', this.shapes.line)
-      .attr('transform', null);
+  this.scales.x.domain([now - (this.n - 2) * this.duration, now - this.duration]);
+  this.scales.y.domain([0, d3.max(this.data)]);
 
-  this.svg.select('.data-stream-area')
-    .attr('d', this.shapes.area)
-    .attr('transform', null);
+  // reset the shapes
+  this.svg.select('.creek-line')
+      .attr('d', this.shapes.line);
+
+  this.svg.select('.creek-area')
+    .attr('d', this.shapes.area);
 
   this.transition(now);
 
   this.data.shift();
 };
 
+Creek.prototype.transitionAxis = function(id){
+  this.axes[id].transition()
+    .duration(this.duration)
+    .ease('linear')
+    .call(this.scales[id].axis);
+};
+
 Creek.prototype.transition = function(now){
-  var self = this, cancelled = false;
+  var self = this;
 
   // Adjust axes and scaling for incoming value
-  ['x', 'y'].map(function(k){
-    self.axes[k].transition()
-      .duration(self.duration)
-      .ease('linear')
-      .call(self.scales[k].axis);
-  });
+  ['x', 'y'].map(this.transitionAxis.bind(this));
 
   // Move the whole world over to expose the next value
-  this.svg.selectAll('path').call(function(p){
+  d3.selectAll(document.querySelectorAll(this.selector + ' .creek-stage path')).call(function(p){
     p.transition()
       .duration(self.duration)
       .ease('linear')
       .attr('transform', 'translate(' + self.scales.x(now - (self.n - 1) * self.duration) + ')')
-      .each('end', function(d, i){
-        if(cancelled === true){
-          cancelled = false;
-        }
-        process.nextTick(function(){
-          if(cancelled === false && self.paused === false){
-            self.tick();
-          }
-        });
-      });
+      .each('end', function(d, i){return (i === 0) ? null : self.tick();});
   });
 };
