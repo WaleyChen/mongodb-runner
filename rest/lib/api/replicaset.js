52928d82e42cd5ac5f98c39c2f96f7de6aa80799
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-29 07:23:42 -0400

    moar replica set
    pseudo code pulled from shell source folks will definitely want.
    thanks @crystalhorn!

--- rest/lib/api/replicaset.js
+++ rest/lib/api/replicaset.js
@@ -1,7 +1,195 @@
 "use strict";
 
-var debug = require('debug')('mg:mongorest:replicaset');
+// @todo: all of the below was debug out of various company repos,
+// trying to figure out what data/operations people actually care about
+// when it comes to replicasets.  it needs to be incorporated into the
+// actual rest api.
+//
+// @todo: from mongo `shell/utils.js`
+//
+// rs.debug.nullLastOpWritten = function(primary, secondary) {
+//     var p = connect(primary+"/local");
+//     var s = connect(secondary+"/local");
+//     s.getMongo().setSlaveOk();
 
+//     var secondToLast = s.oplog.rs.find().sort({$natural : -1}).limit(1).next();
+//     var last = p.runCommand({findAndModify : "oplog.rs",
+//                              query : {ts : {$gt : secondToLast.ts}},
+//                              sort : {$natural : 1},
+//                              update : {$set : {op : "n"}}});
+
+//     if (!last.value.o || !last.value.o._id) {
+//         print("couldn't find an _id?");
+//     }
+//     else {
+//         last.value.o = {_id : last.value.o._id};
+//     }
+
+//     print("nulling out this op:");
+//     printjson(last);
+// };
+
+// rs.debug.getLastOpWritten = function(server) {
+//     var s = db.getSisterDB("local");
+//     if (server) {
+//         s = connect(server+"/local");
+//     }
+//     s.getMongo().setSlaveOk();
+
+//     return s.oplog.rs.find().sort({$natural : -1}).limit(1).next();
+// };
+
+// @todo: extra shell commands pro services use.
+// see https://github.com/10gen/professional-services/blob/master/bin/getMongoData.js
+//
+// print("\n** Replica set config:");
+// printjson(db.getSisterDB("local").system.replset.findOne());
+//
+// DB.prototype.getReplicationInfo = function() {
+//     var db = this.getSiblingDB("local");
+
+//     var result = { };
+//     var oplog;
+//     if (db.system.namespaces.findOne({name:"local.oplog.rs"}) != null) {
+//         oplog = 'oplog.rs';
+//     }
+//     else if (db.system.namespaces.findOne({name:"local.oplog.$main"}) != null) {
+//         oplog = 'oplog.$main';
+//     }
+//     else {
+//         result.errmsg = "neither master/slave nor replica set replication detected";
+//         return result;
+//     }
+
+//     var ol_entry = db.system.namespaces.findOne({name:"local."+oplog});
+//     if( ol_entry && ol_entry.options ) {
+//         result.logSizeMB = ol_entry.options.size / ( 1024 * 1024 );
+//     } else {
+//         result.errmsg  = "local."+oplog+", or its options, not found in system.namespaces collection";
+//         return result;
+//     }
+//     ol = db.getCollection(oplog);
+
+//     result.usedMB = ol.stats().size / ( 1024 * 1024 );
+//     result.usedMB = Math.ceil( result.usedMB * 100 ) / 100;
+
+//     var firstc = ol.find().sort({$natural:1}).limit(1);
+//     var lastc = ol.find().sort({$natural:-1}).limit(1);
+//     if( !firstc.hasNext() || !lastc.hasNext() ) {
+//         result.errmsg = "objects not found in local.oplog.$main -- is this a new and empty db instance?";
+//         result.oplogMainRowCount = ol.count();
+//         return result;
+//     }
+
+//     var first = firstc.next();
+//     var last = lastc.next();
+//     var tfirst = first.ts;
+//     var tlast = last.ts;
+
+//     if( tfirst && tlast ) {
+//         tfirst = DB.tsToSeconds( tfirst );
+//         tlast = DB.tsToSeconds( tlast );
+//         result.timeDiff = tlast - tfirst;
+//         result.timeDiffHours = Math.round(result.timeDiff / 36)/100;
+//         result.tFirst = (new Date(tfirst*1000)).toString();
+//         result.tLast  = (new Date(tlast*1000)).toString();
+//         result.now = Date();
+//     }
+//     else {
+//         result.errmsg = "ts element not found in oplog objects";
+//     }
+
+//     return result;
+// };
+
+// DB.prototype.printReplicationInfo = function() {
+//     var result = this.getReplicationInfo();
+//     if( result.errmsg ) {
+//         if (!this.isMaster().ismaster) {
+//             print("this is a slave, printing slave replication info.");
+//             this.printSlaveReplicationInfo();
+//             return;
+//         }
+//         print(tojson(result));
+//         return;
+//     }
+//     print("configured oplog size:   " + result.logSizeMB + "MB");
+//     print("log length start to end: " + result.timeDiff + "secs (" + result.timeDiffHours + "hrs)");
+//     print("oplog first event time:  " + result.tFirst);
+//     print("oplog last event time:   " + result.tLast);
+//     print("now:                     " + result.now);
+// }
+
+// DB.prototype.printSlaveReplicationInfo = function() {
+//     var startOptimeDate = null;
+
+//     function getReplLag(st) {
+//         assert( startOptimeDate , "how could this be null (getReplLag startOptimeDate)" );
+//         print("\tsyncedTo: " + st.toString() );
+//         var ago = (startOptimeDate-st)/1000;
+//         var hrs = Math.round(ago/36)/100;
+//         print("\t" + Math.round(ago) + " secs (" + hrs + " hrs) behind the primary ");
+//     };
+
+//     function getMaster(members) {
+//         var found;
+//         members.forEach(function(row) {
+//             if (row.self) {
+//                 found = row;
+//                 return false;
+//             }
+//         });
+
+//         if (found) {
+//             return found;
+//         }
+//     };
+
+//     function g(x) {
+//         assert( x , "how could this be null (printSlaveReplicationInfo gx)" )
+//         print("source: " + x.host);
+//         if ( x.syncedTo ){
+//             var st = new Date( DB.tsToSeconds( x.syncedTo ) * 1000 );
+//             getReplLag(st);
+//         }
+//         else {
+//             print( "\tdoing initial sync" );
+//         }
+//     };
+
+//     function r(x) {
+//         assert( x , "how could this be null (printSlaveReplicationInfo rx)" );
+//         if ( x.state == 1 || x.state == 7 ) {  // ignore primaries (1) and arbiters (7)
+//             return;
+//         }
+
+//         print("source: " + x.name);
+//         if ( x.optime ) {
+//             getReplLag(x.optimeDate);
+//         }
+//         else {
+//             print( "\tno replication info, yet.  State: " + x.stateStr );
+//         }
+//     };
+
+//     var L = this.getSiblingDB("local");
+
+//     if (L.system.replset.count() != 0) {
+//         var status = this.adminCommand({'replSetGetStatus' : 1});
+//         startOptimeDate = getMaster(status.members).optimeDate;
+//         status.members.forEach(r);
+//     }
+//     else if( L.sources.count() != 0 ) {
+//         startOptimeDate = new Date();
+//         L.sources.find().forEach(g);
+//     }
+//     else {
+//         print("local.sources is empty; is this db a --slave?");
+//         return;
+//     }
+// }
+
+var debug = require('debug')('mg:mongorest:replicaset');
 var mock = {
   documents: [{
     ok: 1,
