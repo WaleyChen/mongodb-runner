86d2d338023a62be5dade22fec7cdcc7da7990e7
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-29 23:55:38 -0400

    make top way more useful by flipping and using time as the y-axis
    swooonnnn

--- scope/app/controllers/top.js
+++ scope/app/controllers/top.js
@@ -10,47 +10,51 @@ module.exports = Backbone.View.extend({
   initialize: function(){
     this.$el = Backbone.$('#mongoscope');
     this.el = this.$el.get(0);
+    this.$tbody = null;
 
     this.activated = false;
     this.metric = 'lock.count';
 
-    this.top = new models.Top()
-      .on('sync', this.render, this)
-      .on('error', this.render, this);
+    this.top = new models.Top();
 
-    this.graphs = {};
+    this.direction = 'up';
   },
   activate: function(){
+    this.top.on('sync', this.render, this)
     this.top.fetch();
-    this.top.subscribe();
     this.activated = true;
   },
   deactivate: function(){
     this.activated = false;
   },
+  onTopData: function(){
+    var ctx = this.top.toJSON(), html;
+    ctx.update = true;
+    html = this.tpl(ctx);
+
+
+    if(this.direction === 'up'){
+      this.$tbody.prepend(html);
+    }
+    else{
+      this.$tbody.append(html);
+    }
+  },
   render: function(){
     if(!this.activated) return this;
-    var self = this,
-      ctx = self.top.toJSON();
-    ctx.metric = this.metric;
 
-    this.top.get('namespaces').map(function(ns, i){
-      var item = creek('#creek-' + i, {interpolation: 'step-after'});
-      self.graphs[ns] = item;
-      return item;
-    });
+    var ctx = this.top.toJSON(), html;
+    ctx.update = false;
+    html = this.tpl(this.top.toJSON());
 
-    this.top.get('namespaces').map(function(ns){
-      self.graphs[ns].inc(self.top.get('deltas')[ns + '.total.count']);
-    });
+    this.$el.html(html);
+    this.$tbody = this.$el.find('.top-matrix-body');
 
-    if(this.$el.find('.graph-title').length === 0){
-      self.$el.html(self.tpl(ctx));
+    this.top
+      .off('sync', this.render, this)
+      .on('sync', this.onTopData, this);
 
-      this.top.get('namespaces').map(function(ns){
-        self.graphs[ns].render();
-      });
-    }
+    this.top.subscribe();
     return this;
   }
 });
