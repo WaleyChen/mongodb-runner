4bbcd7349c861aa21c30d606fab8f233ce38ed80
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-04-07 16:04:04 -0400

    stubbing in more tests to hit

--- rest/test/index.test.js
+++ rest/test/index.test.js
@@ -17,26 +17,34 @@ function getToken(done){
     });
 }
 
-describe('root', function(){
-  it('should require a token', function(done){
-    get('/api/v1/').expect(401, done);
+describe('rest', function(){
+  var token;
+  before(function(done){
+    getToken(function(err, data){
+      if(err) return done(err);
+      token = data;
+      done();
+    });
   });
 
-  it('should reject a malformed Authorization header', function(done){
-    get('/api/v1/')
-      .set('Authorization', 'open sesame')
-      .expect(400, done);
-  });
+  describe('api root', function(){
+    it('should require a token', function(done){
+      get('/api/v1/').expect(401, done);
+    });
 
-  it('should reject an invalid token', function(done){
-    get('/api/v1/')
-      .set('Authorization', 'Bearer opensesame')
-      .expect(403, done);
-  });
+    it('should reject a malformed Authorization header', function(done){
+      get('/api/v1/')
+        .set('Authorization', 'open sesame')
+        .expect(400, done);
+    });
 
-  it('should show a list of deployments', function(done){
-    debug('generating fixtures');
-    getToken(function(err, token){
+    it('should reject an invalid token', function(done){
+      get('/api/v1/')
+        .set('Authorization', 'Bearer opensesame')
+        .expect(403, done);
+    });
+
+    it('should show a list of deployments', function(done){
       get('/api/v1/')
         .set('Authorization', 'Bearer ' + token)
         .expect(200)
@@ -45,5 +53,23 @@ describe('root', function(){
           done();
         });
     });
+  });
+  describe('host', function(){
+    it('should get instance details');
+    it('should return default profiling info');
+  });
+
+  describe('database', function(){
+    it('should return database details');
+    it('should return in-progress operations');
+    it('should return the oplog');
+  });
+
+  describe('collection', function(){
+    it('should return collection details');
+    it('should be able to run find');
+    it('should be able to run count');
+    it('should be able to run find with explain');
+    it('should be able to run aggregate');
   });
 });
