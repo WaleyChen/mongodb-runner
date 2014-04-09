f34b12f9530abe993542b17e4237357cdc3679e5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-04-09 14:26:50 -0400

    shave off 10MB from the binary by excluding more random things people include in their modules, like you know 7.7MB worth of benchmarking data...

--- bin/mongoscope.js
+++ bin/mongoscope.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 var nconf = require('nconf'),
-  app = require(__dirname + '/../lib'),
-  deployment = require('/../lib/deployment');
+  src = __dirname + '/../lib',
+  app = require(src),
+  deployment = require(src + '/deployment');
 
 // @todo: positional args for seeds.
 deployment.discover(nconf.get('seed'), function(){
