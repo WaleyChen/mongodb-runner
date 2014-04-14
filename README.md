b0c6adeda18edeafb3c3642d77fd5598eb307076
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-04-14 16:03:00 -0400

    [server] consolidate top and log into host.

--- README.md
+++ README.md
@@ -34,3 +34,17 @@ npm test
 npm run-script dist
 ```
 
+## curl party
+
+```
+npm install -g jsontool;
+
+# assuming you dont have auth on and are running mongod
+export TOKEN=`curl -s http://localhost:29017/api/v1/token --data "seed=mongodb://localhost:27017" | json token`;
+
+# top can respond to a normal REST style GET
+curl -X GET "http://localhost:29017/api/v1/localhost:27017/top?access_token=$TOKEN";
+
+# Fun things happen though if we kick the server into server-sent events mode
+curl -X GET -H 'Accept: text/event-stream' "http://localhost:29017/api/v1/localhost:27017/top?access_token=$TOKEN";
+```
--- bin/process-demo.sh
+++ bin/process-demo.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-TAG='v0.0.3';
-DEMOS='~/Dropbox/mongoscope/demos';
+TAG='v0.0.4-top_with_event_stream';
+DEMOS='/Users/lucas/Dropbox/mongoscope/demos';
 
-ffmpeg -i $DEMOS/$TAG.mp4 -vf scale=640:-1 $DEMOS/$TAG-scale.mp4;
-ffmpeg -i $DEMOS/$TAG-scale.mp4 -vf "setpts=0.5*PTS" $DEMOS/$TAG-speed.mp4;
-gify -r 5 $DEMOS/$TAG-speed.mp4 $DEMOS/$TAG.gif;
-rm $DEMOS/$TAG-scale.mp4;
-rm $DEMOS/$TAG-speed.mp4;
+ffmpeg -i $DEMOS/$TAG.mov -vf scale=640:340 $DEMOS/$TAG-scale.mov;
+ffmpeg -i $DEMOS/$TAG-scale.mov -vf "setpts=0.25*PTS" $DEMOS/$TAG-speed.mov;
+gify -r 1 $DEMOS/$TAG-speed.mov $DEMOS/$TAG.gif;
+rm $DEMOS/$TAG-scale.mov;
+rm $DEMOS/$TAG-speed.mov;
