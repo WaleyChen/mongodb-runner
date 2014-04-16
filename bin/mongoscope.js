4b89c2458ed3240812ec5b4f1fe51c985d48c21d
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-04-16 16:05:08 -0400

    [server] bin was still using old `up` setup

--- bin/mongoscope.js
+++ bin/mongoscope.js
@@ -3,14 +3,14 @@
 var nconf = require('nconf'),
   path = require('path'),
   debug = require('debug')('mongoscope:bin'),
-  src = path.resolve(__dirname + '/../lib');
+  src = path.resolve(__dirname + '/../');
 
 debug('hello');
 debug('running from', src);
 
 require(src);
 
-var deployment = require(src + '/deployment');
+var deployment = require(__dirname + '/../lib/deployment');
 
 // @todo: positional args for seeds.
 deployment.discover(nconf.get('seed'), function(){
