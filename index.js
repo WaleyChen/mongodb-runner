0374f67c7c292b0f8a5e98800cb10e92b8f302b4
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-04-16 16:02:51 -0400

    [server] debugging

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var socketio = require('socket.io'),
   app = require('./lib'),
   nconf = require('nconf'),
-  server = require('http').createServer(app);
+  server = require('http').createServer(app),
+  debug = require('debug')('mongoscope');
 
 app.server = server;
 
+debug('trying to get server to listen', nconf.get('listen'));
 server.listen(nconf.get('port'), nconf.get('host'), function(){
-  console.log('listening on ', nconf.get('host') + ':' + nconf.get('port'), arguments);
+  debug('listening on ', nconf.get('host') + ':' + nconf.get('port'), arguments);
 });
 app.emit('io', socketio.listen(server));
 
